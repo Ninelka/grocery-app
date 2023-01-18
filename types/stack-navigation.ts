3b90ec9650ba7607b09881dae514a2738a9770f1
Ninel <ninelgruzdeva@gmail.com>
2023-01-19 02:38:45 +0500

    feat: add type for stack navigation

---
+++ types/stack-navigation.ts
@@ -0,0 +1,4 @@
+import {StackParamList} from "./stack-param-list";
+import {NativeStackNavigationProp} from "@react-navigation/native-stack";
+
+export type StackNavigation = NativeStackNavigationProp<StackParamList>
