957425fc750c6f065e999dc35c055bdae334931a
Ninel <ninelgruzdeva@gmail.com>
2023-01-18 21:18:13 +0500

    feat: add navigators as utils constants

---
+++ utils/Navigators.ts
@@ -0,0 +1,6 @@
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+
+export const Stack = createNativeStackNavigator();
+
+export const BottomTabs = createBottomTabNavigator();
