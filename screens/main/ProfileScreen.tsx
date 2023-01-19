0f3877c7e02bdc46d0c28071cca5177d71a279a0
Ninel <ninelgruzdeva@gmail.com>
2023-01-20 00:21:03 +0500

    feat: add logout btn to profile screen

--- screens/main/ProfileScreen.tsx
+++ screens/main/ProfileScreen.tsx
@@ -1,9 +1,16 @@
-import {Text, View} from "react-native";
+import {SafeAreaView, Text, View} from "react-native";
+import Button from "../../components/UI/Button";
+import {useAuth} from "../../hooks/useAuth";
 
 export function ProfileScreen() {
+    const {logoutHandler} = useAuth();
+
     return (
-        <View>
-            <Text>This is Profile screen</Text>
-        </View>
+        <SafeAreaView style={{flex: 1}}>
+            <View style={{flex: 1}}>
+                <Text>This is Profile screen</Text>
+            </View>
+            <Button onPress={logoutHandler}>Log out</Button>
+        </SafeAreaView>
     );
 }
