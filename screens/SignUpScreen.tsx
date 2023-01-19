9814650b4c2159e42cbedb1c54ab9c401d9d54fe
Ninel <ninelgruzdeva@gmail.com>
2023-01-20 00:19:54 +0500

    feat: put AuthContent to Login and Sign Up screens

--- screens/SignUpScreen.tsx
+++ screens/SignUpScreen.tsx
@@ -1,11 +1,19 @@
 import {Text, View} from "react-native";
+import AuthContent from "../components/Auth/AuthContent";
+import {useAuth} from "../hooks/useAuth";
 
 function SignUpScreen() {
-    return (
-        <View>
-            <Text>This is Sign Up screen</Text>
-        </View>
-    );
+    const {isAuthenticating, signupHandler} = useAuth();
+
+    if (isAuthenticating) {
+        return (
+            // TODO: Replace to Loader component
+            <View>
+                <Text>Creating user...</Text>
+            </View>
+        )
+    }
+    return <AuthContent onAuthenticate={signupHandler} />;
 }
 
 export default SignUpScreen;
