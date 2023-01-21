a57cfc67c75bb634ff8ab2af25ab94ac99321c84
Ninel <ninelgruzdeva@gmail.com>
2023-01-22 01:55:18 +0500

    feat: wrap app with QueryClientProvider

--- App.tsx
+++ App.tsx
@@ -1,14 +1,19 @@
 import { StatusBar } from 'react-native';
 import AuthContextProvider from './src/store/context/auth-context';
 import { Navigation } from './src/components/Navigation';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 export default function App() {
   return (
     <>
       <StatusBar />
-      <AuthContextProvider>
-        <Navigation />
-      </AuthContextProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthContextProvider>
+          <Navigation />
+        </AuthContextProvider>
+      </QueryClientProvider>
     </>
   );
 }
