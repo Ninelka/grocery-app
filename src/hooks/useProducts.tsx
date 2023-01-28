377c2c18b70133dfa463abea72157a1fa2730d02
Ninel <ninelgruzdeva@gmail.com>
2023-01-28 23:30:55 +0500

    feat: create showProductDetailsHandler

--- src/hooks/useProducts.tsx
+++ src/hooks/useProducts.tsx
@@ -1,19 +1,29 @@
 import { useQuery } from 'react-query';
 import { getProducts } from '../../firebase';
+import { useNavigation } from '@react-navigation/native';
+import { StackNavigation } from '../types/stack-navigation';
 
 export const useProducts = () => {
   const { data: products, isLoading: isProductsLoading } = useQuery(
     'products',
     getProducts
   );
+  const navigation = useNavigation<StackNavigation>();
 
   const countAmountWithDiscount = (amount: number, discount: number) => {
     return amount - (amount * discount) / 100;
+  };
+
+  const showProductDetailsHandler = (product) => {
+    navigation.navigate('ProductDetails', {
+      productData: product,
+    });
   };
 
   return {
     products,
     isProductsLoading,
     countAmountWithDiscount,
+    showProductDetailsHandler,
   };
 };
