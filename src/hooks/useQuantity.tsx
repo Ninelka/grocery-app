e905e54b1a414747a2aa891c99351cac4a448c0a
Ninel <ninelgruzdeva@gmail.com>
2023-02-02 11:18:39 +0500

    feat: create useQuantity hook

---
+++ src/hooks/useQuantity.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+
+export const useQuantity = () => {
+  const [quantity, setQuantity] = useState(1);
+
+  const increment = () => {
+    setQuantity((prevState) => prevState + 1);
+  };
+  const decrement = () => {
+    if (quantity > 1) {
+      setQuantity((prevState) => prevState - 1);
+    }
+  };
+
+  const manualChangeQuantity = (inputValue: number) => {
+    setQuantity(+inputValue);
+  };
+
+  return {
+    quantity,
+    increment,
+    decrement,
+    manualChangeQuantity,
+  };
+};
