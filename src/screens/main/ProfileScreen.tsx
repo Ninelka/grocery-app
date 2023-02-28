aff8cb6f219c2aa7fe44e7e758806ee699ceb60b
Ninel <ninelgruzdeva@gmail.com>
2023-02-28 19:20:22 +0500

    feat: put ProfileInfo into ProfileScreen, add divider between info and menu

--- src/screens/main/ProfileScreen.tsx
+++ src/screens/main/ProfileScreen.tsx
@@ -1,12 +1,14 @@
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import { version } from './../../../package.json';
-import ProfileMenu from '../../components/Profile/ProfileMenu';
 import { COLORS, FONT_FAMILY, GlobalStyles } from '../../constants';
+import { ProfileInfo, ProfileMenu } from '../../components/Profile';
 
 export default function ProfileScreen() {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <View style={{ flex: 1 }}>
+    <SafeAreaView style={styles.root}>
+      <View style={styles.content}>
+        <ProfileInfo />
+        <View style={styles.divider}></View>
         <ProfileMenu />
         <Text style={styles.version}>{`ver ${version}`}</Text>
       </View>
@@ -15,11 +17,27 @@ export default function ProfileScreen() {
 }
 
 const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    backgroundColor: COLORS.bgSecondary,
+  },
+  content: {
+    flex: 1,
+    padding: GlobalStyles.spacing.s,
+  },
+  divider: {
+    flex: 1,
+    minHeight: 2,
+    maxHeight: 2,
+    backgroundColor: COLORS.grey5,
+    marginVertical: GlobalStyles.spacing.m,
+  },
   version: {
     textAlign: 'center',
     fontFamily: FONT_FAMILY.semiBold,
     fontWeight: '600',
     fontSize: GlobalStyles.fontSize.callout,
     color: COLORS.grey1,
+    marginTop: GlobalStyles.spacing.s,
   },
 });
