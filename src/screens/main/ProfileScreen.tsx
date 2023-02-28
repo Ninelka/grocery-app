f610a3966038c82286c7c6b5294ea50b8c99d13a
Ninel <ninelgruzdeva@gmail.com>
2023-02-28 15:12:13 +0500

    feat: put ProfileMenu and app version on the ProfileScreen

--- src/screens/main/ProfileScreen.tsx
+++ src/screens/main/ProfileScreen.tsx
@@ -1,16 +1,25 @@
-import { SafeAreaView, Text, View } from 'react-native';
-import Button from '../../components/UI/Button';
-import { useAuth } from '../../hooks/useAuth';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { version } from './../../../package.json';
+import ProfileMenu from '../../components/Profile/ProfileMenu';
+import { COLORS, FONT_FAMILY, GlobalStyles } from '../../constants';
 
 export default function ProfileScreen() {
-  const { logoutHandler } = useAuth();
-
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <View style={{ flex: 1 }}>
-        <Text>This is Profile screen</Text>
+        <ProfileMenu />
+        <Text style={styles.version}>{`ver ${version}`}</Text>
       </View>
-      <Button onPress={logoutHandler}>Log out</Button>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  version: {
+    textAlign: 'center',
+    fontFamily: FONT_FAMILY.semiBold,
+    fontWeight: '600',
+    fontSize: GlobalStyles.fontSize.callout,
+    color: COLORS.grey1,
+  },
+});
