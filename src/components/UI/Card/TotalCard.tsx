e0e71ef430b600d0c52de51abb440ec4fe36647a
Ninel <ninelgruzdeva@gmail.com>
2023-03-22 23:02:08 +0500

    feat: create TotalCard component

---
+++ src/components/UI/Card/TotalCard.tsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import Button from '../Button';
+import { COLORS, FONT_FAMILY, GlobalStyles } from '../../../constants';
+
+interface ITotalCard {
+  counter?: number;
+  onPress?: () => void;
+  isSummary?: boolean;
+  totalAmount?: number;
+  summaryText?: string;
+  summaryAmount?: number;
+}
+
+const TotalCard: React.FC<ITotalCard> = ({
+  onPress,
+  isSummary,
+  counter = 0,
+  totalAmount,
+  summaryText,
+  summaryAmount,
+}) => {
+  return (
+    <Pressable
+      onPress={onPress}
+      style={({ pressed }) => [styles.container, pressed && styles.pressed]}
+    >
+      <View style={{ flex: 1 }}>
+        {isSummary ? (
+          <>
+            <Text style={styles.title}>{`${counter} Item`}</Text>
+            <Text numberOfLines={1} style={styles.summaryText}>
+              {summaryText}
+            </Text>
+          </>
+        ) : (
+          <>
+            <Text style={styles.text}>{`Total (${counter})`}</Text>
+            <Text style={styles.text}>{`$${totalAmount}`}</Text>
+          </>
+        )}
+      </View>
+      {isSummary ? (
+        <Text style={styles.amount}>{`$${summaryAmount}`}</Text>
+      ) : (
+        <Button shape="ellipse">Buy Now</Button>
+      )}
+    </Pressable>
+  );
+};
+
+export default TotalCard;
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: GlobalStyles.spacing.s,
+  },
+  text: {
+    fontFamily: FONT_FAMILY.bold,
+    fontWeight: '700',
+    fontSize: GlobalStyles.fontSize.callout,
+  },
+  title: {
+    fontFamily: FONT_FAMILY.bold,
+    fontWeight: '700',
+    fontSize: GlobalStyles.fontSize.callout,
+    color: COLORS.labelsQuarternary,
+    marginBottom: GlobalStyles.spacing.xs,
+  },
+  summaryText: {
+    fontFamily: FONT_FAMILY.regular,
+    fontWeight: '400',
+    fontSize: GlobalStyles.fontSize.footnote,
+    color: COLORS.labelsQuarternary,
+  },
+  amount: {
+    fontFamily: FONT_FAMILY.bold,
+    fontWeight: '700',
+    fontSize: GlobalStyles.fontSize.title1,
+    color: COLORS.labelsQuarternary,
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
--- src/components/UI/index.ts
+++ src/components/UI/index.ts
@@ -5,6 +5,8 @@ export { default as Link } from './Link/Link';
 export { default as SmallViewBox } from './SmallViewBox/SmallViewBox';
 export { default as CategoryItem } from './CategoryItem';
 export { default as Card } from './Card/Card';
+export { default as FloatingCard } from './Card/FloatingCard';
+export { default as TotalCard } from './Card/TotalCard';
 export { default as Badge } from './Badge/Badge';
 export { default as ProductCard } from './ProductCard/ProductCard';
 export { default as ProductDetailsInfo } from './ProductDetails/ProductDetailsInfo';
