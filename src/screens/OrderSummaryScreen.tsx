f2dc39fbb269a7f5265e9a680cb6d9b1a5b5fc14
Ninel <ninelgruzdeva@gmail.com>
2023-04-10 17:26:37 +0500

    feat: create OrderSummaryScreen

--- src/screens/OrderSummaryScreen.tsx
+++ src/screens/OrderSummaryScreen.tsx
@@ -1,5 +1,60 @@
-import { Text } from 'react-native';
+import { SafeAreaView, StyleSheet, View } from 'react-native';
+import { COLORS, GlobalStyles } from '../constants';
+import { Button, Input } from '../components/UI';
+import { SwipableList, Summary } from '../components/Cart';
+import { useCart } from '../hooks/useCart';
 
 export default function OrderSummaryScreen() {
-  return <Text>This is Order Summary Screen</Text>;
+  const { cartItems } = useCart();
+
+  return (
+    <SafeAreaView style={styles.root}>
+      <View style={styles.container}>
+        <SwipableList items={cartItems} />
+        <View style={styles.divider}></View>
+        <Input
+          label="Promo Code"
+          placeholder="Enter Promo Code"
+          onBtnPress={() => console.log('Input btn pressed!')}
+        />
+        <View style={styles.divider}></View>
+        <Summary />
+      </View>
+      <View style={styles.actionsRow}>
+        <View style={styles.checkoutBtn}>
+          <Button size="large" shape="rounded">
+            Checkout
+          </Button>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    backgroundColor: COLORS.bgSecondary,
+  },
+  container: {
+    flex: 1,
+    padding: GlobalStyles.spacing.s,
+  },
+  divider: {
+    flex: 1,
+    minHeight: 2,
+    maxHeight: 2,
+    backgroundColor: COLORS.grey5,
+    marginVertical: GlobalStyles.spacing.s,
+  },
+  actionsRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: COLORS.bgPrimary,
+    padding: GlobalStyles.spacing.s,
+  },
+  checkoutBtn: {
+    flex: 1,
+  },
+});
