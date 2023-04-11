ffdf07734ffd5e2f03cf27ec61e8a4497e89fc08
Ninel <ninelgruzdeva@gmail.com>
2023-04-11 15:53:09 +0500

    feat: create bottom tabs common types

---
+++ src/types/bottom-tabs-navigation.ts
@@ -0,0 +1,4 @@
+import { BottomTabParamList } from './bottom-tab-param-list';
+import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
+
+export type BottomTabNavigation = BottomTabNavigationProp<BottomTabParamList>;
